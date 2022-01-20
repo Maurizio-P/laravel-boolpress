e41fe2610fe2937a1accc02f14012bce563d73f8
Maurizio-P <mauriziopignotti@gmail.com>
2022-01-20 15:16:22 +0100

    commit requested by project manager

--- resources/views/admin/Post/home.blade.php
+++ resources/views/admin/Post/home.blade.php
@@ -6,10 +6,19 @@
 
 <a href="{{ route('admin.posts.create') }}">Nuovo post</a>
 
-@foreach ($posts as $post)
-    <div class="card mt-4">
-        <h2><a href="{{route('admin.posts.show', $post->id)}}"> {{$post->title}}</a></h2>
-        <p>{{$post->article}}</p>
+<div class="container">
+    @foreach ($posts as $post)
+    <div class="post-container mt-4 d-flex justify-content-between align-items-center" style="border: 1px solid black; background-color:white">
+        <div class="text-post">
+            
+            <h2>{{$post->title}}</h2>
+            <p>{{$post->article}}</p>
+        </div>
+        <div class="link-post">
+            <a class="mx-4" href="{{route('admin.posts.show', $post->id)}}"> Mostra post</a>
+            <a class="mx-4" href="{{route('admin.posts.edit', $post->id)}}"> Modifica post</a>
+        </div>
     </div>
-@endforeach
+    @endforeach
+</div>
 @endsection
